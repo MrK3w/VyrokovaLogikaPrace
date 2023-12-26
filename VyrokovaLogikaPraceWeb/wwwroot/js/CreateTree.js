8e77e8e70d08dac74f6c32b296c141699c42e275
MrK3w <jakubpietrus2000@gmail.com>
2023-12-26 19:26:46 +0100

    added function for adding new nodes or removing nodes

--- VyrokovaLogikaPraceWeb/wwwroot/js/CreateTree.js
+++ VyrokovaLogikaPraceWeb/wwwroot/js/CreateTree.js
@@ -3,6 +3,67 @@ function escapeRegExp(string) {
     return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }
 
+function handleButtonZmenaTextuClick() {
+    // Perform your action here
+    var inputValue = $('#TreeInput').val();
+    var validSymbols = ['∧', '∨', '≡', '¬', '⇒', '¬¬'];
+    var regex = new RegExp('^[a-zA-Z]+$|^(' + validSymbols.map(escapeRegExp).join('|') + ')$');
+    if (regex.test(inputValue)) {
+        if (globalInput) globalInput.text(inputValue);
+    } else {
+        alert("Nespravný vstup. Zadej pouze literál nebo logickou spojku!");
+    }
+}
+
+function handleButtonOdstranNoduClick() {
+
+    if (globalInput) {
+        globalInput.closest('li').remove();
+    }
+}
+
+function handleButtonPridejNoduClick() {
+
+    // Find the span element
+    var spanElement = document.querySelector('.tf-nc[style="border-color: blue;"]');
+    console.log(spanElement);
+    if (spanElement) {
+;
+        // Find the nearest ul element
+        var nearestUl = spanElement.closest('ul');
+        console.log(nearestUl);
+        if (nearestUl) {
+            // Create a new ul element and append it after the nearest ul
+            var newUl = document.createElement('ul');
+            var newLi = document.createElement('li');
+            var newSpan = document.createElement('span');
+            newSpan.className = 'tf-nc';
+            newSpan.textContent = ' ▭ ';
+            newLi.appendChild(newSpan);
+            newUl.appendChild(newLi);
+
+            //added new node 
+            spanElement.insertAdjacentElement('afterend',newUl)
+
+            // Check if the nearestUl has a next sibling before inserting
+            $(".tf-nc").on("click", function () {
+                // Set it to an empty string to reset to default
+                $(".tf-nc").css("border-color", "");
+                //saving current node into global property
+                globalInput = $(this);
+                //setting current node into blue border
+                $(this).css("border-color", "blue");
+            });
+            
+          
+        } else {
+            console.error('No parent ul found for the span element.');
+        }
+    } else {
+        console.error('Span element not found.');
+    }
+}
+
 var globalInput; // Declare globalInput
 
 function showDialog(title, message) {
@@ -14,32 +75,27 @@ function showDialog(title, message) {
 // Case of interactive tree
 $(document).ready(function () {
 
-    // Buttons for text
+    // Buttons for adding symbols into texbox
     $(".insert-button").click(function () {
         var buttonText = $(this).data("text");
         $("#TreeInput").val(buttonText);
     });
 
-    document.getElementById('submitButton').addEventListener('click', function () {
-        // Perform your action here
-        var inputValue = $('#TreeInput').val();
-        var validSymbols = ['∧', '∨', '≡', '¬', '⇒', '¬¬'];
-        var regex = new RegExp('^[a-zA-Z]+$|^(' + validSymbols.map(escapeRegExp).join('|') + ')$');
-        console.log("Input value: " + inputValue);
-        if (regex.test(inputValue)) {
-            if (globalInput) globalInput.text(inputValue);
-        } else {
-            alert("Invalid Input. Please enter only alphabetic letters, '∧', '¬', or '¬¬'.");
-        }
-    });
+    //button for 'Zmena textu'
+    document.getElementById('zmenaTextuButton').addEventListener('click', handleButtonZmenaTextuClick);
 
+    //button for 'Odstra nodu'
+    document.getElementById('odstranNoduButton').addEventListener('click', handleButtonOdstranNoduClick);
+
+    document.getElementById('pridejNoduButton').addEventListener('click', handleButtonPridejNoduClick);
+
+    //function to add into nodes click function
     $(".tf-nc").on("click", function () {
-        // Get the current span element
-        $(".tf-nc").css("border-color", "");  // Set it to an empty string to reset to default
-        var spanElement = $(this);
-        console.log("HTML type of $(this):", $(this).prop("nodeName"));
-        globalInput = spanElement;
-        console.log(globalInput);
+        // Set it to an empty string to reset to default
+        $(".tf-nc").css("border-color", "");
+        //saving current node into global property
+        globalInput = $(this);
+        //setting current node into blue border
         $(this).css("border-color", "blue");
     });
 })
