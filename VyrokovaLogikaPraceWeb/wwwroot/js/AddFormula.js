fdaf53ccc1402be3509b9d493e40e90f6eb62961
MrK3w <jakubpietrus2000@gmail.com>
2024-01-18 18:27:14 +0100

    added buttons to add formula

---
+++ VyrokovaLogikaPraceWeb/wwwroot/js/AddFormula.js
@@ -0,0 +1,31 @@
+﻿function handleButtonPridejFormuliClick() {
+    //Get value from input
+    var inputValue = $('#FormulaInput').val();
+
+    //check if input is valid
+    var validSymbols = ['∧', '∨', '≡', '¬', '⇒', '¬¬'];
+    var regex = new RegExp('^[a-zA-Z]+$|^(' + validSymbols.map(escapeRegExp).join('|') + ')$');
+    if (regex.test(inputValue)) {
+    } else {
+        alert("Nespravný vstup. Zadej pouze literál nebo logickou spojku!");
+    }
+}
+
+// Case of interactive tree
+$(document).ready(function () {
+    // Buttons for adding symbols into texbox
+    $(".insert-button").click(function () {
+        var buttonText = $(this).data("text");
+        var formulaInput = $("#FormulaInput");
+        var cursorPos = formulaInput.prop("selectionStart");
+        var currentValue = formulaInput.val();
+
+        var newValue =
+            currentValue.substring(0, cursorPos) + buttonText + currentValue.substring(cursorPos);
+
+        formulaInput.val(newValue);
+    });
+
+    //button for 'Pridej formuli'
+    document.getElementById('pridejFormuliButton').addEventListener('click', handleButtonPridejFormuliClick);
+})
