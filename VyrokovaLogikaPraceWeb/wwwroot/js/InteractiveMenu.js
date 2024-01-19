c4377bede84ff9fa84e00fac4b983cc262c317f2
MrK3w <jakubpietrus2000@gmail.com>
2024-01-19 19:27:39 +0100

    removed replicated include of jquery, redesigned menu

---
+++ VyrokovaLogikaPraceWeb/wwwroot/js/InteractiveMenu.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $('#syntacticTreeDropdown, #syntacticTreeSubMenu').hover(function () {
+        $('#syntacticTreeSubMenu').addClass('show');
+    }, function () {
+        $('#syntacticTreeSubMenu').removeClass('show');
+    });
+
+$('#dagDropdown, #dagSubMenu').hover(function () {
+    $('#dagSubMenu').addClass('show');
+    }, function () {
+    $('#dagSubMenu').removeClass('show');
+    });
+});
+
+// Redirect on click
+$('#syntacticTreeDropdown').on('click', function () {
+    window.location.href = '/SyntacticTree';
+});
+
+// Redirect on click for DAG
+$('#dagDropdown').on('click', function () {
+    window.location.href = '/DAG';
+});
