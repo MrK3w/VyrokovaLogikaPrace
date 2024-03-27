d2f3b3820977ad132d7484e6d9c9110fee2f228d
MrK3w <jakubpietrus2000@gmail.com>
2024-03-27 20:02:31 +0100

    added more funcionality into CreateDAG

--- VyrokovaLogikaPraceWeb/wwwroot/js/CreateDAG.js
+++ VyrokovaLogikaPraceWeb/wwwroot/js/CreateDAG.js
@@ -66,23 +66,33 @@ function createGraph()
 }
 
 function addNewNode(newNodeId, newNodeLabel, newNodeTitle, color) {
+    const x = Math.random() * 100; // Adjust 1000 to the width of your container
+    const y = Math.random() * 100; // Adjust 1000 to the height of your container
     const newNode = {
         id: newNodeId,
         label: newNodeLabel,
         title: newNodeTitle,
-        color: { background: color }
+        color: { background: color },
+        parentId: 0,
+        x: x,
+        y: y,
+        side: 'nevim'
     };
     // Add the new node to the DataSet
     data.nodes.add(newNode);
 }
 
-function addNewEdge(NodeId, parentId, color) {
+function addNewEdge(nodeId, parentId, color) {
     const newEdge = {
-        from: NodeId,
+        from: nodeId,
         to: parentId,
         arrows: 'to',
         color: { color: color } // Example color, customize as needed
     };
+    const existingNode = data.nodes.get(nodeId);
+    existingNode.parentId = parentId;
+    data.nodes.update(existingNode);
+    console.log(data.nodes.get());
     data.edges.add(newEdge);
 }
 
@@ -122,10 +132,11 @@ function handleButtonVytvorFormuliClick() {
 }
 
 function getFormulaFromNodes() {
-
-    /*  while (!haveAllNodesFilledTitle(nodes)) {*/
     var nodes = data.nodes.get();
     var edges = data.edges.get();
+    removeTitles();
+    while (!haveAllNodesFilledTitle(nodes)) {
+
     console.log(nodes);
     console.log(edges);
         for (let i = 0; i < nodes.length; i++) {
@@ -133,20 +144,56 @@ function getFormulaFromNodes() {
                 var nodesWithParentId = getNodesByParentId(nodes, nodes[i].id)
                 if (haveChildNodesFilledTitle(nodesWithParentId))
                 {
-                    var neededEdges = getEdges(edges, nodesWithParentId);
-                    modifyNodes(nodes, nodes[i], nodesWithParentId, neededEdges);
+                    getSide(edges, nodesWithParentId);
+                    modifyNodes(nodes[i], nodesWithParentId);
                 }
             }
         }
-    /*}*/
+    }
+    var divElement = document.getElementById("zmeny");
+
+    // Check if the <div> element exists
+    if (divElement) {
+        // Construct the text to be displayed
+        var newText = "Formule je " + data.nodes.get(1).title;
+
+        // Update the content of the <div> element
+        divElement.textContent = newText;
+    } else {
+        console.error("Element with ID 'zmeny' not found.");
+    }
+}
+
+function removeTitles() {
+    var nodes = data.nodes.get();
+    for (let i = 0; i < nodes.length; i++) {
+        if (!isAlphabet(nodes[i].title)) {
+            const existingNode = data.nodes.get(nodes[i].id);
+            existingNode.title = '▭';
+            data.nodes.update(existingNode);
+        }
+    }
 }
 
 function getNodesByParentId(nodes, parentId) {
     return nodes.filter(node => node.parentId === parentId);
 }
 
-function getEdges(edges, nodesWithParentId) {
-    console.log("STOP");
+function getSide(edges, nodesWithParentId) {
+    for (let i = 0; i < nodesWithParentId.length; i++) {
+        for (let j = 0; j < edges.length; j++) {
+            if (nodesWithParentId[i].id === edges[j].from && nodesWithParentId[i].parentId === edges[j].to) {
+                const existingNode = nodesWithParentId[i];
+                if (edges[j].color.color === 'orange') existingNode.side = 'right';
+                else if (edges[j].color.color === 'blue') existingNode.side = 'left';
+                // Assuming you have some mechanism to update the node in your own data structure
+                // For example, if nodesWithParentId is an array, you can directly update the node:
+                nodesWithParentId[i] = existingNode;
+                // If nodesWithParentId is an object or Map, update it accordingly
+            }
+        }
+    }
+
 }
 
 function haveAllNodesFilledTitle(nodes) {
@@ -164,9 +211,36 @@ function haveChildNodesFilledTitle(nodes) {
     return true;
 }
 
-function modifyNodes(nodes, node, nodesWithParentId, edges) {
+function modifyNodes(node, nodesWithParentId) {
+    if (node.label != "¬" && node.label != "¬¬") {
+        if (nodesWithParentId[0].side == "left") {
+            node.title = '(' + nodesWithParentId[0].title + node.label + nodesWithParentId[1].title + ')';
+        }
+        else {
+            node.title = '(' + nodesWithParentId[1].title + node.label + nodesWithParentId[0].title + ')';
+        }
+    }
+    else {
+        if (isAlphabet(nodesWithParentId[0].label)) {
+            node.title = node.label + nodesWithParentId[0].label;
+        }
+        else {
+            node.title =  node.label + '(' + nodesWithParentId[0].label + ')';
+        }
+    }
+    const existingNode = data.nodes.get(node.id);
+    existingNode.title = node.title;
+    data.nodes.update(existingNode);
+    return;
 
 }
+
+function isAlphabet(input) {
+    // Regular expression to match only alphabets (case-insensitive)
+    const regex = /^[a-zA-Z]+$/;
+    return regex.test(input);
+}
+
 
 async function handleButtonPridejVetev(color) {
 
@@ -179,6 +253,25 @@ async function handleButtonPridejVetev(color) {
                 resolve(params.nodes[0]);
             });
         });
+        if (nodeId == globalInput.id) {
+            alert("Uzel nemůže navázat sám na sebe!");
+            return;
+        }
+
+        const edges = data.edges.get();
+
+        for (let i = 0; i < edges.length; i++) {
+            if (edges[i].to == nodeId) {
+                console.log(edges[i].color);
+                if (edges[i].color.color == color) {
+                    if (color == 'blue')
+                        alert("Tento uzel už má levou větev.");
+                    else alert('Tento uzel už má pravou větev. ');
+                    return;
+                }
+            }
+        }
+
         addNewEdge(globalInput.id, nodeId, color);
         clickedSecondNode = false;
     }
